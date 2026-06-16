dd299ce2a92d72185169de1c8b8993ab1836b8f2
vijimca-15 <vijiviju161@gmail.com>
2026-06-16 10:14:26 +0530

    App.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import InvoiceForm from "./components/InvoiceForm";
 
-
+// This is App.jsx
 function App() {
   return (
     <>
